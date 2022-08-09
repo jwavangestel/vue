6a60ef3aa7bf81916e4a85563ba6fe6384dcb535
jwavangestel <64045996+jwavangestel@users.noreply.github.com>
2022-08-09 20:22:26 +0200

    First

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+
+</script>
+
+<template>
+  <div id="app">
+    <div id="nav">
+      <RouterLink to="/">Events</RouterLink> | 
+      <RouterLink to="/about">About</RouterLink>
+    </div>
+    <router-view></router-view>
+  </div>
+</template>
+
+<style scoped>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+#nav {
+  padding: 30px;
+}
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+h4 {
+  font-size: 20px;
+}
+</style>
